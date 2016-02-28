e31c4a76a8173ef73b0d6abce3261f827d3243ec
Matt Todd <matt.c.todd@gmail.com>
2016-02-28 17:01:33 -0600

    refactored out the query from the handler and into the model

--- app/handlers/pairing_session_handler.rb
+++ app/handlers/pairing_session_handler.rb
@@ -1,8 +1,8 @@
 class PairingSessionHandler
-  attr_accessor :ids
+  attr_reader :user_ids
 
-  def initialize(ids)
-    @ids = ids
+  def initialize(user_ids)
+    @user_ids = user_ids
   end
 
   def create_or_update_session
@@ -13,11 +13,7 @@ class PairingSessionHandler
   private
 
   def session_already_exists?
-    if previous_session == nil
-      return false
-    else
-      return previous_session.end_time > threshold
-    end
+    previous_session != nil and previous_session.end_time > threshold
   end
 
   def threshold
@@ -29,27 +25,7 @@ class PairingSessionHandler
   end
 
   def previous_session
-    @previous_session ||= PairingSession.find_by_sql(crazy_sql).first
-  end
-
-  def crazy_sql
-    <<-SQL
-        select ps.*
-        from pairing_sessions ps
-        where ps.id in (
-           select number_of_pairs_in_sessions.pairing_session_id
-           from 
-           (
-              select psu.pairing_session_id, count(*) as number_pairs
-              from pairing_sessions_users psu
-              where psu.user_id in (#{ids.join(',')})
-              group by psu.pairing_session_id
-           ) number_of_pairs_in_sessions
-           where number_of_pairs_in_sessions.number_pairs > 1
-        )
-        order by ps.end_time desc
-        limit 1
-    SQL
+    @previous_session ||= PairingSession.last_session_for user_ids
   end
 
   def create_session
@@ -57,6 +33,6 @@ class PairingSessionHandler
   end
 
   def users
-    User.where id: ids
+    User.where id: user_ids
   end
 end
