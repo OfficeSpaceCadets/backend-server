de097d59ff3d296098549845d0cbbd962f2f32a8
Matt Todd <matt.c.todd@gmail.com>
2016-02-28 06:33:56 -0600

    updated docs on auth and seed data to have a token

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 #Backend Server
 
+This is the backend server for the Office Space Cadets' Hackathon project. This is currently deployed to: *https://osc-backend.herokuapp.com*
+
+##Authentication
+
+Authentcation requires an HTTP header to be present in each request. 
+
+    Authorization: Token token=<token goes here>
+
+The authorized tokens for production are found in the api\_tokens database. The database seed file provides the token '24ee441f6823271610ea6c4e57d8541b'.
+
 ##API
 
 The API calls will require authentication via a token in the HTTP header of each request.
 
-### PUT /ping
+### POST /ping
 
 This is the session creation and keep alive for all pairing sessions.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-matt = User.create name: 'Matt', username: '', email: ''
-bj = User.create name: 'BJ', username: '', email: ''
-lee = User.create name: 'Lee', username: '', email: ''
-rosemary = User.create name: 'Rosemary', username: '', email: ''
+matt = User.create! name: 'Matt', username: '', email: ''
+bj = User.create! name: 'BJ', username: '', email: ''
+lee = User.create! name: 'Lee', username: '', email: ''
+rosemary = User.create! name: 'Rosemary', username: '', email: ''
 
-PairingSession.create users: [matt, bj], start_time: 1.hour.ago, end_time: 90.minutes.ago
-PairingSession.create users: [bj, lee], start_time: 20.minutes.ago, end_time: Time.now
-PairingSession.create users: [rosemary, matt], start_time: 5.minutes.ago, end_time: Time.now
+PairingSession.create! users: [matt, bj], start_time: 1.hour.ago, end_time: 90.minutes.ago
+PairingSession.create! users: [bj, lee], start_time: 20.minutes.ago, end_time: Time.now
+PairingSession.create! users: [rosemary, matt], start_time: 5.minutes.ago, end_time: Time.now
+
+ApiToken.create! token: '24ee441f6823271610ea6c4e57d8541b'
