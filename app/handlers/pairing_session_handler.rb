d66b7da21b9070aaeed6d2ec992818ca4fe70c79
Matt Todd <matt.c.todd@gmail.com>
2016-02-28 16:52:26 -0600

    pings within 30 seconds of eachother with the same users are all within the same pairing session

--- app/handlers/pairing_session_handler.rb
+++ app/handlers/pairing_session_handler.rb
@@ -6,10 +6,55 @@ class PairingSessionHandler
   end
 
   def create_or_update_session
-    PairingSession.create! users: users, start_time: Time.now, end_time: Time.now
+    return create_session unless session_already_exists?
+    update_session
   end
 
   private
+
+  def session_already_exists?
+    if previous_session == nil
+      return false
+    else
+      return previous_session.end_time > threshold
+    end
+  end
+
+  def threshold
+    30.seconds.ago
+  end
+
+  def update_session
+    previous_session.update_attributes end_time: Time.now
+  end
+
+  def previous_session
+    @previous_session ||= PairingSession.find_by_sql(crazy_sql).first
+  end
+
+  def crazy_sql
+    <<-SQL
+        select ps.*
+        from pairing_sessions ps
+        where ps.id in (
+           select number_of_pairs_in_sessions.pairing_session_id
+           from 
+           (
+              select psu.pairing_session_id, count(*) as number_pairs
+              from pairing_sessions_users psu
+              where psu.user_id in (#{ids.join(',')})
+              group by psu.pairing_session_id
+           ) number_of_pairs_in_sessions
+           where number_of_pairs_in_sessions.number_pairs > 1
+        )
+        order by ps.end_time desc
+        limit 1
+    SQL
+  end
+
+  def create_session
+    PairingSession.create! users: users, start_time: Time.now, end_time: Time.now
+  end
 
   def users
     User.where id: ids
--- spec/handlers/pairing_session_handler_spec.rb
+++ spec/handlers/pairing_session_handler_spec.rb
@@ -3,13 +3,33 @@ require 'rails_helper'
 RSpec.describe PairingSessionHandler do
   let!(:user1) { create :user }
   let!(:user2) { create :user }
+  let!(:user3) { create :user }
   let(:user_ids) { [user1.id, user2.id] }
+  let!(:current_time) { Time.now }
 
   subject { described_class.new user_ids }
 
+  
+  shared_examples 'created a valid session' do
+    it 'should create a session that contains the users' do
+      actual_user_ids = pairing_session.users.pluck :id
+      
+      expect(actual_user_ids).to eq(user_ids)
+    end
+
+    it 'should create session with a current start time' do
+      expect(pairing_session.start_time.to_i).to eq(current_time.to_i)
+    end
+
+    it 'should create session with a the same end time as the start time' do
+      expect(pairing_session.start_time).to eq(pairing_session.end_time)
+    end
+  end
+
   describe '#create_or_update_session' do
     context 'no previous session exists' do
-      let(:current_time) { 30.days.ago }
+      let(:pairing_session) { PairingSession.first }
+
       before do
         Timecop.freeze current_time
         subject.create_or_update_session
@@ -23,21 +43,79 @@ RSpec.describe PairingSessionHandler do
         expect(PairingSession.count).to be(1)
       end
 
-      it 'should create a session that contains the users' do
-        actual_user_ids = PairingSession.first.users.pluck :id
-        
-        expect(actual_user_ids).to eq(user_ids)
+      it_behaves_like 'created a valid session'
+    end
+
+    context 'previous session exists but was a while ago' do
+      let(:pairing_session) { PairingSession.last }
+      
+      before do
+        Timecop.freeze(current_time - threshold) do
+          subject.create_or_update_session
+        end
+
+        Timecop.freeze current_time
+        subject.create_or_update_session
       end
 
-      it 'should create session with a current start time' do
-        expect(PairingSession.first.start_time.to_i).to eq(current_time.to_i)
+      after do
+        Timecop.return
+      end
+      
+      it 'should create a second session' do
+        expect(PairingSession.count).to be(2)
       end
 
-      it 'should create session with a the same end time as the start time' do
-        session = PairingSession.first
+      it_behaves_like 'created a valid session'
+    end
 
-        expect(session.start_time).to eq(session.end_time)
+    context 'previous session exists only a few seconds ago but with different users' do
+      let(:pairing_session) { PairingSession.last }
+      
+      before do
+        Timecop.freeze current_time
+
+        described_class.new([user1.id, user3.id]).create_or_update_session
+        subject.create_or_update_session
+      end
+
+      after do
+        Timecop.return
+      end
+      
+      it 'should create two sessions' do
+        expect(PairingSession.count).to be(2)
+      end
+    end
+
+    context 'previous session exists only a few seconds ago' do
+      let(:pairing_session) { PairingSession.first }
+      
+      before do
+        Timecop.freeze(current_time - (threshold - 1.second)) do
+          subject.create_or_update_session
+        end
+
+        Timecop.freeze current_time
+        subject.create_or_update_session
+      end
+
+      after do
+        Timecop.return
+      end
+      
+      it 'should only create a single session' do
+        expect(PairingSession.count).to be(1)
+      end
+
+      it 'should update the end_time to be the current time' do
+        expect(pairing_session.end_time.to_i).to be(current_time.to_i)
       end
     end
   end
 end
+
+def threshold
+  30.seconds
+end
+
