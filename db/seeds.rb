7c9bc6f3ba2b266d4b574e6c30dc2396b9435456
Matt Todd <matt.c.todd@gmail.com>
2016-02-29 07:31:33 -0600

    rounded out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 matt = User.create! name: 'Matt', username: '', email: ''
 bj = User.create! name: 'BJ', username: '', email: ''
@@ -14,5 +7,6 @@ rosemary = User.create! name: 'Rosemary', username: '', email: ''
 PairingSession.create! users: [matt, bj], start_time: 1.hour.ago, end_time: 90.minutes.ago
 PairingSession.create! users: [bj, lee], start_time: 20.minutes.ago, end_time: Time.now
 PairingSession.create! users: [rosemary, matt], start_time: 5.minutes.ago, end_time: Time.now
+PairingSession.create! users: [lee, matt], start_time: 1.minute.ago, end_time: Time.now
 
 ApiToken.create! token: '24ee441f6823271610ea6c4e57d8541b'
