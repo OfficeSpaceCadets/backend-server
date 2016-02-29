c3494bac7e625922655da73722f9d228a98d612f
Matt Todd <matt.c.todd@gmail.com>
2016-02-29 07:38:09 -0600

    thinking not good yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 
+ApiToken.create! token: '24ee441f6823271610ea6c4e57d8541b'
+
 matt = User.create! name: 'Matt', username: '', email: ''
 bj = User.create! name: 'BJ', username: '', email: ''
 lee = User.create! name: 'Lee', username: '', email: ''
 rosemary = User.create! name: 'Rosemary', username: '', email: ''
 
-PairingSession.create! users: [matt, bj], start_time: 1.hour.ago, end_time: 90.minutes.ago
-PairingSession.create! users: [bj, lee], start_time: 20.minutes.ago, end_time: Time.now
-PairingSession.create! users: [rosemary, matt], start_time: 5.minutes.ago, end_time: Time.now
-PairingSession.create! users: [lee, matt], start_time: 1.minute.ago, end_time: Time.now
+PairingSession.create! users: [bj, lee], start_time: 1.hour.ago, end_time: 90.minutes.ago
+PairingSession.create! users: [lee, matt], start_time: 20.minutes.ago, end_time: Time.now
+PairingSession.create! users: [matt, rosemary], start_time: 5.minutes.ago, end_time: Time.now
+PairingSession.create! users: [rosemary, bj], start_time: 1.minute.ago, end_time: Time.now
 
-ApiToken.create! token: '24ee441f6823271610ea6c4e57d8541b'
