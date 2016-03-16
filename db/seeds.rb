098d4de5ea51f7fbbf483ad32911487c2b19a90f
Matt Todd <matt.c.todd@gmail.com>
2016-03-15 21:06:19 -0500

    added external_ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 
 ApiToken.create! token: '24ee441f6823271610ea6c4e57d8541b'
 
-matt = User.create! name: 'Matt', username: '', email: ''
-bj = User.create! name: 'BJ', username: '', email: ''
-lee = User.create! name: 'Lee', username: '', email: ''
-rosemary = User.create! name: 'Rosemary', username: '', email: ''
+matt = User.create! name: 'Matt', username: 'toddm', email: 'matt.todd@asynchrony.com', external_id: '34_29_88_94_57'
+bj = User.create! name: 'BJ', username: 'selfb', email: 'bj.self@asynchrony.com', external_id: '178_130_195_169_90'
+lee = User.create! name: 'Lee', username: 'esnerl', email: 'lee.esner@asynchrony.com', external_id: '149_151_178_8_184'
+rosemary = User.create! name: 'Rosemary', username: 'kaskowitzr', email: 'rosemary.kaskoitz@asynchrony.com', external_id: '116_209_145_226_214'
 
 PairingSession.create! users: [bj, lee], start_time: 1.hour.ago, end_time: 90.minutes.ago
 PairingSession.create! users: [lee, matt], start_time: 20.minutes.ago, end_time: Time.now
