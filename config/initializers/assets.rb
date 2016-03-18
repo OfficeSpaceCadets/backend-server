84ce6963ffbeb278fd9a09c6e6ac88a1d369a796
Matt Todd <matt.c.todd@gmail.com>
2016-03-18 05:07:00 -0500

    cleaned up how we were importing the css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
+Rails.application.config.assets.precompile += %w( latest_sessions.scss home.scss )
 Rails.application.config.assets.precompile += %w( latest_sessions.js home.js )
