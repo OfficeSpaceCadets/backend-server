e4a2dfe6627be26a19116453ecd31107cb26285a
Matt Todd <matt.c.todd@gmail.com>
2016-03-18 15:53:30 -0500

    make sure our tests include people working by themselves

--- spec/controllers/api/todays_session_controller_spec.rb
+++ spec/controllers/api/todays_session_controller_spec.rb
@@ -5,16 +5,9 @@ RSpec.describe Api::TodaysSessionsController do
     let!(:user1) { create :user }
     let!(:user2) { create :user }
     let!(:user3) { create :user }
-    let!(:session1) { create :pairing_session, start_time: 20.minutes.ago, end_time: 15.minutes.ago }
-    let!(:session2) { create :pairing_session, start_time: 30.minutes.ago, end_time: 20.minutes.ago }
-
-    before do
-      session1.users = [user1, user2]
-      session2.users = [user2, user3]
-
-      session1.save
-      session2.save
-    end
+    let!(:session1) { create :pairing_session, start_time: 20.minutes.ago, end_time: 15.minutes.ago, users: [user1, user2] }
+    let!(:session2) { create :pairing_session, start_time: 30.minutes.ago, end_time: 20.minutes.ago, users: [user1, user2] }
+    let!(:session3) { create :pairing_session, start_time: 24.hours.ago, end_time: 23.hours.ago, users: [user3] }
 
     it 'returns http success' do
       get :index
@@ -22,31 +15,53 @@ RSpec.describe Api::TodaysSessionsController do
       expect(response).to have_http_status(:success)
     end
 
-    it 'should contain all pairing sessions' do
-      get :index
+    context 'fetching the pairing data' do
+      before do
+        get :index
+      end
 
-      pairing_sessions = JSON.parse(response.body)
+      it 'should contain all pairing sessions' do
+        pairing_sessions = JSON.parse(response.body)
 
-      expect(pairing_sessions.count).to eq(2)
-    end
+        expect(pairing_sessions.count).to eq(3)
+      end
 
-    it 'should transform the pairing sessions' do
-      get :index
+      context 'two people working together' do
+        it 'should transform the pairing sessions' do
+          expected_object = { 
+            'users' => [ 
+              {'external_id' => user1.external_id, 'name' => user1.name}, 
+              {'external_id' => user2.external_id, 'name' => user2.name} 
+            ],  
+            'start_time' => session1.start_time.strftime('%m/%d/%Y %H:%M:%S'),
+            'end_time' => session1.end_time.strftime('%m/%d/%Y %H:%M:%S'),
+            'duration' => '5 minutes', 
+            'pair_clock' => '00:05:00' 
+          }
 
-      expected_object = { 
-        'users' => [ 
-          {'external_id' => user1.external_id, 'name' => user1.name}, 
-          {'external_id' => user2.external_id, 'name' => user2.name} 
-        ],  
-        'start_time' => session1.start_time.strftime('%m/%d/%Y %H:%M:%S'),
-        'end_time' => session1.end_time.strftime('%m/%d/%Y %H:%M:%S'),
-        'duration' => '5 minutes', 
-        'pair_clock' => '00:05:00' 
-      }
+          pairing_sessions = JSON.parse(response.body)
 
-      pairing_sessions = JSON.parse(response.body)
+          expect(pairing_sessions.first).to eq(expected_object)
+        end
+      end
 
-      expect(pairing_sessions.first).to eq(expected_object)
+      context 'one person working by themself' do
+        it 'should transform the pairing session' do
+          expected_object = { 
+            'users' => [ 
+              {'external_id' => user3.external_id, 'name' => user3.name}
+            ],  
+            'start_time' => session3.start_time.strftime('%m/%d/%Y %H:%M:%S'),
+            'end_time' => session3.end_time.strftime('%m/%d/%Y %H:%M:%S'),
+            'duration' => 'about 1 hour', 
+            'pair_clock' => '01:00:00' 
+          }
+
+          pairing_sessions = JSON.parse(response.body)
+
+          expect(pairing_sessions.last).to eq(expected_object)
+        end
+      end
     end
   end
 end
