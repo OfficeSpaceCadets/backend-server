56a76bccfc70ab1f70d56411e909aa3884862b0f
Matt Todd <matt.c.todd@gmail.com>
2016-03-18 13:04:15 -0500

    reverted the pairing clock

--- app/assets/javascripts/latest_sessions.js
+++ app/assets/javascripts/latest_sessions.js
@@ -1,6 +1,6 @@
 (function() {
   fetch_latest_session();
-  setInterval(fetch_latest_session, 5000);
+  setInterval(fetch_latest_session, 1000);
 
   function fetch_latest_session() {
     $.getJSON('/api/latest_session').done(populate_pairing_data);
@@ -15,34 +15,6 @@
 
     $('.latest_session .duration').empty().append(data['duration']);
     $('.latest_session .start_time').empty().append(data['start_time']);
-    // clock_timer = setInterval(update_clock(data['start_time']), 1000);
-    update_clock(data['start_time']);
+    $('.pair-clock').empty().append(data['pair_clock']);
   }
-
-  function update_clock(start_time) {
-    var now = new Date();
-    var countFrom = new Date(start_time);
-    var difference = now - countFrom;
-
-    var days=Math.floor(difference/(60*60*1000*24)*1);
-    var hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
-    var mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
-    var secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
-
-    var output = hours + ':' + pad(mins) + ':' + pad(secs);
-    $('.pair-clock').empty().append(output);
-  }
-
-  function pad(val)
-    {
-      var valString = val + "";
-      if(valString.length < 2)
-      {
-        return "0" + valString;
-      }
-      else
-      {
-        return valString;
-      }
-    }
 })();
