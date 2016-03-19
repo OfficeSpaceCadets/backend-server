3a40d79f3715cc6edc1babbc3c46ede3cd745043
bj.self <bj.self@asynchrony.com>
2016-03-18 21:01:49 -0500

    added js, styles, and seed data for displaying a single person on the latest pair page

--- app/assets/javascripts/latest_sessions.js
+++ app/assets/javascripts/latest_sessions.js
@@ -7,11 +7,23 @@
   }
 
   function populate_pairing_data(data) {
+    console.log("Users: ", data['users']);
     $('.pair-clock').empty().append(data['pair_clock']);
-    $('.photos .photo:first img').attr('src', '/images/' + data['users'][0]['external_id'] + '.jpg');
-    $('.photos .photo:first figcaption').empty().append(data['users'][0]['name']);
-    $('.photos .photo:last img').attr('src', '/images/' + data['users'][1]['external_id'] + '.jpg');
-    $('.photos .photo:last figcaption').empty().append(data['users'][1]['name']);
+    if (data['users'][0]) {
+      $('.photos .photo:first img').attr('src', '/images/' + data['users'][0]['external_id'] + '.jpg');
+      $('.photos .photo:first figcaption').empty().append(data['users'][0]['name']);
+      $('.photos .photo:first').removeClass('inactive');
+    } else {
+      $('.photos .photo:first').addClass('inactive');
+    }
+
+    if (data['users'][1]) {
+      $('.photos .photo:last img').attr('src', '/images/' + data['users'][1]['external_id'] + '.jpg');
+      $('.photos .photo:last figcaption').empty().append(data['users'][1]['name']);
+      $('.photos .photo:last').removeClass('inactive');
+    } else {
+      $('.photos .photo:last').addClass('inactive');
+    }
 
     $('.latest_session .duration').empty().append(data['duration']);
     $('.latest_session .start_time').empty().append(data['start_time']);
